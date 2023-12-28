7c867edb6a0537d7540a5af59395a2b6cdfab227
Muhammad Saim Sajid <141317850+saimsajid123@users.noreply.github.com>
2023-12-28 21:28:42 +0500

    Update Scheme of study management by Saim.py

--- Scheme of study management by Saim.py
+++ Scheme of study management by Saim.py
@@ -31,12 +31,46 @@
 
 import tkinter as tk
 import re
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from bson import ObjectId
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/sos_manager'
+mongo = PyMongo(app)
+
 
 class SOSManager:
     def __init__(self):
+        # Initialize MongoDB
+        self.db = mongo.db
+        self.sos_collection = self.db.sos_collection
+
+        # Initialize Tkinter
         self.root = tk.Tk()
         self.root.title("Scheme Of Study Manager")
 
+        # Initialize semesters attribute
+        self.semesters = {}
+
+        # Tkinter GUI components
+        self.subject_entry = tk.Entry(self.root, width=100)
+        self.course_code_entry = tk.Entry(self.root, width=100)
+        self.teacher_name_entry = tk.Entry(self.root, width=100)
+        self.description_entry = tk.Entry(self.root, width=100)
+        self.sos_listbox = tk.Listbox(self.root, width=60)
+        self.selected_semester = tk.StringVar(self.root)
+        self.semester_listbox = tk.OptionMenu(self.root, self.selected_semester, *self.get_semester_options())
+
+        # Tkinter buttons
+        add_button = tk.Button(self.root, text="Add", command=self.add_item)
+        remove_button = tk.Button(self.root, text="Remove", command=self.remove_item)
+        edit_button = tk.Button(self.root, text="Edit", command=self.edit_item)
+        save_button = tk.Button(self.root, text="Save", command=self.save_sos)
+        sort_button = tk.Button(self.root, text="Sort", command=self.sort_items)
+        load_button = tk.Button(self.root, text="Load Subjects", command=self.load_subjects)
+
+        # Tkinter menu
         menubar = tk.Menu(self.root)
         self.root.config(menu=menubar)
 
@@ -46,70 +80,47 @@ class SOSManager:
         teacher_menu.add_command(label="Sir Rafaqat Kazmi", command=lambda: self.select_teacher(2))
         teacher_menu.add_command(label="Ma'am Sunnia", command=lambda: self.select_teacher(3))
 
-        self.semesters = {}
-
-        subject_label = tk.Label(self.root, text="Subject Name:")
-        subject_label.grid(row=0, column=0)
-
-        self.subject_entry = tk.Entry(self.root, width=100)
+        # Tkinter grid layout
         self.subject_entry.grid(row=0, column=1, columnspan=3)
-
-        course_code_label = tk.Label(self.root, text="Course Code:")
-        course_code_label.grid(row=1, column=0)
-
-        self.course_code_entry = tk.Entry(self.root, width=100)
         self.course_code_entry.grid(row=1, column=1, columnspan=3)
-
-        teacher_name_label = tk.Label(self.root, text="Teacher Name:")
-        teacher_name_label.grid(row=2, column=0)
-
-        self.teacher_name_entry = tk.Entry(self.root, width=100)
         self.teacher_name_entry.grid(row=2, column=1, columnspan=3)
-
-        description_label = tk.Label(self.root, text="Description:")
-        description_label.grid(row=3, column=0)
-
-        self.description_entry = tk.Entry(self.root, width=100)
         self.description_entry.grid(row=3, column=1, columnspan=3)
-
-        add_button = tk.Button(self.root, text="Add", command=self.add_item)
         add_button.grid(row=4, column=0)
-
-        remove_button = tk.Button(self.root, text="Remove", command=self.remove_item)
         remove_button.grid(row=4, column=1)
-
-        edit_button = tk.Button(self.root, text="Edit", command=self.edit_item)
         edit_button.grid(row=4, column=2)
-
-        save_button = tk.Button(self.root, text="Save", command=self.save_sos)
         save_button.grid(row=4, column=3)
-
-        sort_button = tk.Button(self.root, text="Sort", command=self.sort_items)
         sort_button.grid(row=4, column=4)
-
-        self.sos_listbox = tk.Listbox(self.root, width=60)
         self.sos_listbox.grid(row=5, column=0, columnspan=5)
-
-        semester_label = tk.Label(self.root, text="Select Semester:")
-        semester_label.grid(row=6, column=0)
-
-        self.selected_semester = tk.StringVar(self.root)
-        self.semester_listbox = tk.OptionMenu(self.root, self.selected_semester, *self.get_semester_options())
         self.semester_listbox.grid(row=6, column=1, columnspan=2)
-
-        load_button = tk.Button(self.root, text="Load Subjects", command=self.load_subjects)
         load_button.grid(row=6, column=3, columnspan=2)
 
-        try:
-            with open("sos.txt", "r") as f:
-                for line in f:
-                    self.sos_listbox.insert(tk.END, line.strip())
-        except FileNotFoundError:
-            pass
-
+        # Tkinter event bindings
         self.sos_listbox.bind("<Double-Button-1>", self.edit_item)
 
-        self.get_semester_options()
+        # Flask routes
+        @app.route('/sos', methods=['GET'])
+        def get_sos():
+            sos_items = self.get_sos_from_mongo()
+            return jsonify(sos_items)
+
+        @app.route('/sos', methods=['POST'])
+        def add_sos():
+            data = request.json
+            self.add_sos_to_mongo(data)
+            return jsonify({"message": "SOS item added successfully"})
+
+        @app.route('/sos/<item_id>', methods=['PUT'])
+        def update_sos(item_id):
+            data = request.json
+            self.update_sos_in_mongo(item_id, data)
+            return jsonify({"message": "SOS item updated successfully"})
+
+        @app.route('/sos/<item_id>', methods=['DELETE'])
+        def delete_sos(item_id):
+            self.delete_sos_from_mongo(item_id)
+            return jsonify({"message": "SOS item deleted successfully"})
+
+        # Run the Tkinter and Flask application
         self.root.mainloop()
 
     def get_semester_options(self):
@@ -184,27 +195,20 @@ class SOSManager:
                 self.sos_listbox.insert(tk.END, course)
 
     def select_teacher(self, teacher_number):
-
-        print(f"Selected Teacher {teacher_number}")
-
- 
         self.sos_listbox.delete(0, tk.END)
 
-
         if teacher_number == 1:
-
             self.semesters = {"Semester 1": ["Subject1: Description1 (Code1, Sir Nauman)"],
-                          "Semester 2": ["Subject2: Description2 (Code2, Sir Nauman)"]}
+                              "Semester 2": ["Subject2: Description2 (Code2, Sir Nauman)"]}
         elif teacher_number == 2:
-
             self.semesters = {"Semester 1": ["Subject3: Description3 (Code3, Sir Rafaqat Kazmi)"],
-                          "Semester 2": ["Subject4: Description4 (Code4, Sir Rafaqat Kazmi)"]}
+                              "Semester 2": ["Subject4: Description4 (Code4, Sir Rafaqat Kazmi)"]}
         elif teacher_number == 3:
- 
             self.semesters = {"Semester 1": ["Subject5: Description5 (Code5, Ma'am Sunnia)"],
-                          "Semester 2": ["Subject6: Description6 (Code6, Ma'am Sunnia)"]}
+                              "Semester 2": ["Subject6: Description6 (Code6, Ma'am Sunnia)"]}
 
         self.update_sos_listbox()
+
 
 if __name__ == "__main__":
     sos_manager = SOSManager()
